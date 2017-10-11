9cac29bfde70c7b2423bcfb11800f350da3c1a27
Seb <voop.sd@gmail.com>
2017-10-11 10:50:28 +1030

    Added design folder

---
+++ Assets/Design/Scripts/characterController2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController2 : MonoBehaviour {
+    private CharacterController controller;
+    public float speed = 10;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Input
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+
+        if (!controller.isGrounded) {
+            //fall
+            move.y += Physics.gravity.y * 10 *  Time.fixedDeltaTime;
+        } else {
+            move.y = 0;
+        }
+
+        //Move
+        controller.Move(move * Time.fixedDeltaTime);
+    }
+}
