95c403fb8f5488809a3e8a34c4a8b051e497999d
AIE\s171463 <sloth.13@hotmail.com>
2017-10-30 15:04:21 +1030

    Added pickup system, fixed up some weird things, still no melee.. working on it

--- Assets/Scotts/Scripts/CharacterControllerTest.cs
+++ Assets/Scotts/Scripts/CharacterControllerTest.cs
@@ -14,25 +14,32 @@ public class CharacterControllerTest : MonoBehaviour {
     private float m_MAXSPEED = 5;
     
     //Melee
-    public float meleeCooldown = 5;
+    public float meleeCooldown = 0.5f;
     private float meleeTimer = 0;
     public float meleeMod = 1.0f;
     public float meleeRange = 1.0f;
+    public float meleeAngle = 90; //Degrees
 
     //Ranged
     public float shootCooldown = 5;
     private float shootTimer  =0;
     public GameObject bulletTest;
+
+    //Inventory
+    private List<string> m_keys;
+    public int m_scrap =0;
+    public int m_mutagen =0;
 	// Use this for initialization
 	void Start () {
         controller = GetComponent<CharacterController>();
+        m_keys = new List<string>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //Input
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
-        if(move.magnitude >= m_MAXSPEED) {
+        if(move.sqrMagnitude >= m_MAXSPEED*m_MAXSPEED) {
             move = move.normalized * m_MAXSPEED;
         }
         transform.LookAt(getMouseToPlayerPlanePoint()); //look at mouse
@@ -44,7 +51,6 @@ public class CharacterControllerTest : MonoBehaviour {
         } else {
             move.y = 0;
         }
-        
 
         //Move
         controller.Move(move * Time.deltaTime);
@@ -53,18 +59,28 @@ public class CharacterControllerTest : MonoBehaviour {
         if(Input.GetButton("Fire1") && shootTimer >= shootCooldown && bulletTest != null) {
             Instantiate<GameObject>(bulletTest, this.transform.position, this.transform.rotation);//make transform postition the point on the gun
             shootTimer = 0;
-        }else if(shootTimer < shootCooldown){
-            shootTimer += Time.deltaTime;
-        }else if(Input.GetButton("Fire2") && meleeTimer >= meleeCooldown) {
+        }//Melee
+        else if(Input.GetButton("Fire2") && meleeTimer >= meleeCooldown) {
             //Sphere case in front?
             Debug.Log("Melee");
+            //
+            //RaycastHit hit;
+
+            //
             meleeTimer = 0;
-        }else if(meleeTimer < meleeCooldown) {
-            meleeTimer = 0;
+        }
+        //Timers
+        if (shootTimer < shootCooldown) {
+            shootTimer += Time.deltaTime;
+        }
+        if (meleeTimer < meleeCooldown) {
+            meleeTimer += Time.deltaTime;
         }
         
     }
 
+
+    //Used to use mouse on board
     private Vector3 getMouseToPlayerPlanePoint() {
 
         Vector3 mousePos = Input.mousePosition;
@@ -77,5 +93,20 @@ public class CharacterControllerTest : MonoBehaviour {
 
         Vector3 castPoint = mouseRay.GetPoint(rayDist);
         return castPoint;
+    }
+
+    void OnTriggerEnter(Collider c) {
+        //Items
+        if(c.transform.tag == "Item") {
+            string itemName = c.GetComponent<ItemScriptOne>().m_info;
+            if (itemName.Contains("Key")){
+                m_keys.Add(itemName);
+            }else if(itemName == "Scrap") {
+                m_scrap++;
+            }else if(itemName == "MutaGen") {
+                m_mutagen++;
+            }
+            Destroy(c.gameObject);
+        }
     }
 }
