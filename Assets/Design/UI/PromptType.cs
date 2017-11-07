4064b49df9bf961178aa8f7390d9e4459d4dcc52
Seb <voop.sd@gmail.com>
2017-11-07 12:13:10 +1030

    Updated Open Barrel model and Barrel Prefab

--- Assets/Design/UI/PromptType.cs
+++ Assets/Design/UI/PromptType.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class PromptType : MonoBehaviour {
 
-    public enum Type { OPEN };
+    public enum Type { OPEN, USE };
     public Type typeofprompt = Type.OPEN;
     public Text promptText;
 
 	void Start () {
         promptText = transform.GetComponentInChildren<Text>();
         if (typeofprompt == Type.OPEN)
-        promptText.text = "Open";		
+        promptText.text = "Open";
+        if (typeofprompt == Type.USE)
+        promptText.text = "Use";		
 	}
 }
