71fe30328b4ab9f796d588b3b13ae1c17c2fa16e
Seb <voop.sd@gmail.com>
2017-11-21 12:32:39 +1030

    Added wood texture, changed light material, added sound to door animation (will fix later)

---
+++ Assets/Design/Scripts/PlaySound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlaySound : MonoBehaviour {
+    AudioSource audio;
+
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Play()
+    {
+        audio.Play();
+    }
+
+}
