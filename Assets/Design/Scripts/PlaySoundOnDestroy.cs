f0f81d59b70043a4ac7c1cc93aef23f48a8790bf
Bax <voop.sd@gmail.com>
2017-11-23 14:31:56 +1030

    Adjusted footstep sfx, Created PlaySoundonDestroy script, Added sfx to key and mutagen, Added model and sfx to scrap, Organised firebarrel prefab

---
+++ Assets/Design/Scripts/PlaySoundOnDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnDestroy : MonoBehaviour {
+	GameObject soundmanager;
+	AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+		sound = GetComponent<AudioSource>();
+	}
+
+    void OnDestroy()
+    {
+		soundmanager = new GameObject();
+		AudioSource audioSource = soundmanager.AddComponent<AudioSource>() as AudioSource;
+		audioSource.clip = sound.clip;
+		audioSource.Play();
+		Destroy (soundmanager, 5.0f);
+    }
+}
