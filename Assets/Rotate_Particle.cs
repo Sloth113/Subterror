5180a37836dfb70bda51ce5a5ba983d0466804cf
AIE\s164856 <liam.fosdike@hotmail.com>
2017-11-28 16:02:18 +1030

    added explosion particle effects and corosive/healing/block particle effects

---
+++ Assets/Rotate_Particle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Particle : MonoBehaviour {
+	public float rotateSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.back * Time.deltaTime * rotateSpeed);
+	}
+}
