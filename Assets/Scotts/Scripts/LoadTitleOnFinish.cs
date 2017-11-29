6877c3ff7e96ccab75c7ae3e403b58ff3dadf927
Scott <sloth.13@hotmail.com>
2017-11-29 18:16:52 +1030

    Load title script on video player.

---
+++ Assets/Scotts/Scripts/LoadTitleOnFinish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class LoadTitleOnFinish : MonoBehaviour {
+    VideoPlayer m_intro;
+	// Use this for initialization
+	void Start () {
+        m_intro = GetComponent<VideoPlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_intro.frame == (long) m_intro.frameCount) {
+            //Video has finshed playing..
+            SceneManager.LoadScene("TitleScreen");
+        }
+    }
+}
